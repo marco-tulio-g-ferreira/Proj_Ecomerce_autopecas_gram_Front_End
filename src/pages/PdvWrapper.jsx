6a74a19d86bd26c2770cdcb95af60a4bc5507ec3
marco-tulio-g-ferreira <gomesferreiramarcotulio@gmail.com>
2026-07-02 15:26:18 -0300

    outra correção de bugs

--- src/pages/PdvWrapper.jsx
+++ src/pages/PdvWrapper.jsx
@@ -1,13 +1,12 @@
 import CheckoutPDV from "./CheckoutPDV";
+import api from "../services/api";
 
 export default function PdvWrapper() {
   // Função que busca no servidor sem paginação local
   const buscarProdutoNoServidor = async (termo) => {
     try {
-      const response = await fetch(`http://10.0.0.107:8000/api/products/?search=${encodeURIComponent(termo)}`);
-      if (!response.ok) throw new Error('Erro na rede');
-      const data = await response.json();
-      return data.results || data || [];
+      const response = await api.get(`products/?search=${encodeURIComponent(termo)}`);
+      return response.data.results || response.data || [];
     } catch (err) {
       console.error("Erro na busca do servidor:", err);
       return [];
